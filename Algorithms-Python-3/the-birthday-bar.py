718328156553522b2084f7fc5fa8d578d013f97b
Daglar Duman <daglarduman510@gmail.com>
2022-11-30 14:03:19 +0300

    Add files via upload

---
+++ Algorithms-Python-3/the-birthday-bar.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    ans=0
+    for i in range(len(s)):
+        n=0
+        count=0
+        while(n<(m)):
+            count+=s[i+n]
+            n+=1
+        if(count==d):
+            ans+=1
+        if(i+n==len(s)):
+            break
+    return ans
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = sys.stdout
+
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
